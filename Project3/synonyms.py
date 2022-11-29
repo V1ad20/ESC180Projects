b3b16cdf7e5e706708e7676c3915dacd021c4b9e
Brian Kang <66335932+BrianK0120@users.noreply.github.com>
2022-11-29 14:53:09 -0500

    finished build_semantic_descriptors

--- Project3/synonyms.py
+++ Project3/synonyms.py
@@ -21,17 +21,41 @@ def cosine_similarity(vec1, vec2):
     pass
 
 
+# dict = {"a": 1, "b": 2, "c": 3}
+# print(list(dict.keys()))
+# print(list(dict.values()))
+# print(list(dict.items()))
+
+
 def build_semantic_descriptors(sentences):
-    pass
+    d = {}
+    for sentence in sentences:
+        for word in sentence:
+            if not(word in list(d.keys())):
+                d[word] = {}
+            for sameword in sentence:
+                if not(sameword == word):
+                    if not(sameword in list(d[word].keys())):
+                        d[word][sameword] = 1
+                    else:
+                        d[word][sameword] += 1
+    return d
+
+L = [["i", "am", "a", "sick", "man"],
+["i", "am", "a", "spiteful", "man"],
+["i", "am", "an", "unattractive", "man"],
+["i", "believe", "my", "liver", "is", "diseased"],
+["however", "i", "know", "nothing", "at", "all", "about", "my",
+"disease", "and", "do", "not", "know", "for", "certain", "what", "ails", "me"]]
+print(build_semantic_descriptors(L))
+
+
 
 def build_semantic_descriptors_from_files(filenames):
     pass
 
-
-
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
     pass
-
 
 def run_similarity_test(filename, semantic_descriptors, similarity_fn):
     pass
