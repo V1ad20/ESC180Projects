4583c5b780ae7efebc1cb63957d42992a87a1f69
Brian Kang <66335932+BrianK0120@users.noreply.github.com>
2022-11-29 15:33:09 -0500

    implemented build_semantic_descriptors_from_files

--- Project3/synonyms.py
+++ Project3/synonyms.py
@@ -54,12 +54,14 @@ def build_semantic_descriptors(sentences):
 def build_semantic_descriptors_from_files(filenames):
     filtered_sentences = []
     for filename in filenames:
-        text = open(filename, "r", econding = "latin1").read()
-        text = text.replace("!", ".").replace("?", ".")
+        text = open(filename, "r", encoding = "latin1").read()
+        text = text.replace("!", ".").replace("?", ".").replace("\n"," ")
         sentences = text.split(". ")
         for sentence in sentences:
             filtered_sentences.append(sentence.replace(",","").replace(":","").replace(";","").replace("--","").replace("-","").replace("  "," ").split(" "))
     return build_semantic_descriptors(filtered_sentences)
+print()
+print(build_semantic_descriptors_from_files(["test.txt"]))
 
 def most_similar_word(word, choices, semantic_descriptors, similarity_fn):
     pass
